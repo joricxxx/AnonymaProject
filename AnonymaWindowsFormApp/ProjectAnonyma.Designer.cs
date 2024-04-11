e5ff16312b892e4d80c610ce16e6cb0293184fc9
Joerick Amadora <Joerick Amadora@DESKTOP-I539J3P>
2024-04-11 09:58:02 +0800

    UI Project setup using Windows Form Application

---
+++ AnonymaWindowsFormApp/ProjectAnonyma.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace AnonymaWindowsFormApp
+{
+    partial class ProjectAnonyma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ProjectAnonyma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 681);
+            this.Font = new System.Drawing.Font("Ebrima", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "ProjectAnonyma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProjectAnonyma";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
