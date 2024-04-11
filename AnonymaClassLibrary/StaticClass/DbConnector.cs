98a603447d08e884fd27d098dd9d3a4972711e7b
Joerick Amadora <Joerick Amadora@DESKTOP-I539J3P>
2024-04-11 09:36:36 +0800

    Retrieving content and determining valid key functunality was added

--- AnonymaClassLibrary/StaticClass/DbConnector.cs
+++ AnonymaClassLibrary/StaticClass/DbConnector.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AnonymaClassLibrary.Model;
+using FireSharp.Response;
 
 namespace AnonymaClassLibrary.StaticClass
 {
@@ -28,6 +29,36 @@ namespace AnonymaClassLibrary.StaticClass
         {
             string path = $"SecretMessages/{Key}";
             var response = await client.SetTaskAsync(path, message.serialize());
+        }
+        /// <summary>
+        /// This will determine if the key exist inside the database
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool KeyIsValid(string key)
+        {
+            string path = $"SecretMessages/{key}";
+            FirebaseResponse response = client.Get(path);
+            if (response != null && response.Body != "null")
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// To used this method, you must ensure that the key is valid or else
+        /// it will return null value string
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string retrieveContent(string key)
+        {
+            string path = $"SecretMessages/{key}";
+            if(KeyIsValid(key))
+            {
+                FirebaseResponse response = client.Get(path);
+                var dictionary = response.ResultAs<Dictionary<string, string>>();
+                return Convert.ToString(dictionary["Content"]);
+            }
+            return "null";
         }
     }
 }
