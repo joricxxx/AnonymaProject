f04b93ab9f596ff028dc26a561265679e91f9758
Joerick Amadora <Joerick Amadora@DESKTOP-I539J3P>
2024-04-11 09:46:04 +0800

    Add internet checker static class to prevent uploading, retrieving data from firebase when theres no internet connection

---
+++ AnonymaClassLibrary/StaticClass/IntenetConnectionChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymaClassLibrary.StaticClass
+{
+    public static class IntenetConnectionChecker
+    {
+        public static bool IsConnectedToInternet()
+        {
+            try
+            {
+                using (Ping ping = new Ping())
+                {
+                    PingReply reply = ping.Send("8.8.8.8", 1000); 
+                    return reply.Status == IPStatus.Success;
+                }
+            }
+            catch (Exception)
+            {
+                return false; 
+            }
+        }
+    }
+}
