751031af4bd56b55b051539bccc96eafb7ddf7d0
Joerick Amadora <Joerick Amadora@DESKTOP-I539J3P>
2024-04-11 09:21:45 +0800

    Add Database Connection using the Firesharp

---
+++ AnonymaClassLibrary/StaticClass/DbConnector.cs
@@ -0,0 +1,21 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymaClassLibrary.StaticClass
+{
+    public static class DbConnector
+    {
+        private static IFirebaseConfig config { get; set; } = new FirebaseConfig
+        {
+            AuthSecret = "dpJHVdb7UxaZKz18Ij92gEY3vKzwPybUBohcLaED",
+            BasePath = "https://anonymadb-74fac-default-rtdb.firebaseio.com/"
+        };
+        private static IFirebaseClient client { get; set; } = new FirebaseClient(config);
+    }
+}
