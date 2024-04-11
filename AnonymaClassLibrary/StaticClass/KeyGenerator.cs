1ee6f9fc2eef836d8f3caa9ee3b950211fdbb937
Joerick Amadora <Joerick Amadora@DESKTOP-I539J3P>
2024-04-11 09:42:59 +0800

    Add a random key generator

---
+++ AnonymaClassLibrary/StaticClass/KeyGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymaClassLibrary.StaticClass
+{
+    public static class KeyGenerator
+    {
+        public static string GenerateKey()
+        {
+            const int length = 30;
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            Random random = new Random();
+
+            char[] randomChars = new char[length];
+            for (int i = 0; i < length; i++)
+                randomChars[i] = chars[random.Next(chars.Length)];
+            return new string(randomChars);
+        }
+    }
+}
