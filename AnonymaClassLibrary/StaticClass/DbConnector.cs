6eed8b9cdabebe7be61dcbbec707016bff0084f5
Joerick Amadora <Joerick Amadora@DESKTOP-I539J3P>
2024-04-11 09:26:13 +0800

    Addition of upload function

--- AnonymaClassLibrary/StaticClass/DbConnector.cs
+++ AnonymaClassLibrary/StaticClass/DbConnector.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AnonymaClassLibrary.Model;
 
 namespace AnonymaClassLibrary.StaticClass
 {
@@ -17,5 +18,16 @@ namespace AnonymaClassLibrary.StaticClass
             BasePath = "https://anonymadb-74fac-default-rtdb.firebaseio.com/"
         };
         private static IFirebaseClient client { get; set; } = new FirebaseClient(config);
+        /// <summary>
+        /// method to upload message into the firebase
+        /// </summary>
+        /// <param name="Key">The  key means the identifier of the message,
+        /// this is randomly generated and composed of 30 characters</param>
+        /// <param name="message"></param>
+        public static async void uploadMessage(string Key, Message message)
+        {
+            string path = $"SecretMessages/{Key}";
+            var response = await client.SetTaskAsync(path, message.serialize());
+        }
     }
 }
