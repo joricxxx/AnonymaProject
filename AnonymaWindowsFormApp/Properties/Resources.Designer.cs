0edf1c0396c1549f9ec97e73f2a724bf91cd7adf
Joerick Amadora <Joerick Amadora@DESKTOP-I539J3P>
2024-04-22 11:20:00 +0800

    Image for Readmi

--- AnonymaWindowsFormApp/Properties/Resources.Designer.cs
+++ AnonymaWindowsFormApp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace AnonymaWindowsFormApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap anonyma_logo {
+            get {
+                object obj = ResourceManager.GetObject("anonyma-logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cancel_button {
             get {
                 object obj = ResourceManager.GetObject("cancel_button", resourceCulture);
